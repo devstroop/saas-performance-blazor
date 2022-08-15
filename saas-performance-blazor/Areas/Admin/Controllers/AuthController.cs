7752e251f629df0da092c14a90f910906ccf6ce5
Akash Shaw <44998751+itsalfredakku@users.noreply.github.com>
2022-08-16 01:53:05 +0530

    Add project files.

---
+++ saas-performance-blazor/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace saas_performance_blazor.Areas.Admin.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public async Task<IActionResult> Login([FromForm] string Email, string Password, string CallBackRoute = "/")
+        {
+            if(Email == "a@a.com" && Password == "0000")
+            {
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Role, "Admin")
+                };
+                ClaimsIdentity identity = new ClaimsIdentity(claims);
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(claimsPrincipal);
+            }
+            return Redirect(CallBackRoute);
+        }
+    }
+}
